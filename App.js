e20a0ad281c4693db17d1c030252be9c3fbcc969
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-01-13 23:53:26 +0530

    Add styles

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+import React, { Component } from "react";
+import { Image, View, ScrollView, StyleSheet, Platform, Dimensions, StatusBar, FlatList } from "react-native";
+import { Body, Header, Text, Title, Container, Content, Button, H3, Card, CardItem, Icon, Thumbnail, Spinner } from "native-base";
+import { web } from "react-native-communications";
+import {
+  ReactiveBase,
+  MultiDropdownList,
+  SingleDropdownList,
+  ReactiveList
+} from "@appbaseio/reactivebase-native";
+
+var { height, width } = Dimensions.get('window');
+if (Platform.OS === 'android') {
+  height = height - StatusBar.currentHeight;
 }
+
+const primaryColor = "#3cb371";
+
+const commons = {
+  padding1: {
+    padding: 10
+  },
+  padding2: {
+    padding: 20
+  },
+  padding3: {
+    padding: 30
+  },
+  padding4: {
+    padding: 40
+  },
+  padding5: {
+    padding: 50
+  },
+}
+
+const S = {
+  fullWidth: {
+    width: width,
+  },
+  container: {
+    flex: 1,
+  },
+  column: {
+    flexDirection: 'column',
+  },
+  alignCenter: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 0
+  },
+  header: {
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "white",
+  },
+  controls: {
+    ...commons.padding2,
+    paddingTop: 0,
+    backgroundColor: primaryColor,
+    alignItems: "stretch",
+  },
+  results: {
+    ...commons.padding2,
+  },
+  none: {
+    display: "none"
+  },
+  flex: {
+    display: "flex"
   },
 });
+
